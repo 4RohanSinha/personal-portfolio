435b8e315d94f09093f0d4749941f543c6ec44e9
4RohanSinha <rohansinha1@yahoo.com>
2024-08-25 11:34:46 +0530

    added handling to remove console messages in prod mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
 
+if (import.meta.env.MODE === 'production') {
+  console.log = () => {};
+  console.debug = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
